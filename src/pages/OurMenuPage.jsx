4bc0148495cb874e2279c679eb1a56fdbb6a562f
Chalani Dissanayaka <15415@coderacademy.edu.au>
2024-12-06 19:36:48 +1100

    Implemented Footer

--- src/pages/OurMenuPage.jsx
+++ src/pages/OurMenuPage.jsx
@@ -1,8 +1,8 @@
-export default function OurMenuPage(){
-
-	return(
-		<div>
-			<h1>OurMenu Page</h1>
-		</div>
-	);
+export default function OurMenuPage() {
+  return (
+    // Please add your code which related to "Ourmenu-Page" below
+    <div>
+      <h1>OurMenu Page</h1>
+    </div>
+  );
 }
