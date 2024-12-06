26b45cebf4a4dbf62b47f11d47123946eda50b8e
Chalani Dissanayaka <15415@coderacademy.edu.au>
2024-12-06 21:12:42 +1100

    Revert "Merge pull request #2 from bean-buzz/feature-frontend-3003-create-the-footer"
    This reverts commit 648f7fb11dc5dbf57215fe4fe37e4a42c88371e2, reversing
    changes made to 9d20c22f9af0b0dfdcb0bdb7cf826da16192e7ef.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,8 @@
-import "../styles/HomePage.css";
+export default function HomePage(){
 
-export default function HomePage() {
-  return (
-    // Please add your code which related to "Home-Page" below
-    <div className="main">
-      <h1>Home Page</h1>
-    </div>
-  );
+	return(
+		<div>
+			<h1>Home Page</h1>
+		</div>
+	);
 }
