bd10294e2a6e81463786c0173e32d21348753ba5
Chalani Dissanayaka <15415@coderacademy.edu.au>
2024-12-06 22:04:48 +1100

    Updated HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,8 +3,11 @@ import "../styles/HomePage.css";
 export default function HomePage() {
   return (
     // Please add your code which related to "Home-Page" below
-    <div className="main">
-      <h1>Home Page</h1>
-    </div>
+    <>
+      <section className="main">
+        <h1>Home Page</h1>
+      </section>
+      <section></section>
+    </>
   );
 }
