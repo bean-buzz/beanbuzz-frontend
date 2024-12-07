56af6818509ad1d9a73aa6d973110d7e130e01b3
rday45 <rahularvinddayal@gmail.com>
2024-12-07 19:47:55 +1100

    started working on menu and menu-item cards. Added a new dietary tag feature

---
+++ src/components/Menu.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import MenuItemCard from "./MenuItemCard";
+import "../styles/Menu.css"
+
+export default function Menu() {
+
+  const [menuItems, setMenuItems] = useState([]);
+
+// This allows the menu component to fetch menu-data when the component first loads. This initial fetch may be 'All Items' later
+  useEffect(() => {
+    async function fetchMenuItemData() {
+      try {
+        const response = await fetch("http://localhost:3000/lunchItems");
+        const data = await response.json();
+        if (data) {
+          setMenuItems(data);
+        }
+      } catch (error) {
+        console.error("Error fetching menu items:", error);
+      }
+    }
+    fetchMenuItemData();
+  }, []);
+
+  console.log(menuItems);
+
+  return (
+    <div className="menu">
+        <h1>hello</h1>
+      {menuItems.map((item)=>{
+        return <MenuItemCard key={item._id} menuItemObj={item}>hi</MenuItemCard>
+      })}
+    </div>
+  );
+}
--- src/pages/OurMenuPage.jsx
+++ src/pages/OurMenuPage.jsx
@@ -1,8 +1,11 @@
+import Menu from "../components/Menu";
+
 export default function OurMenuPage(){
 
 	return(
 		<div>
 			<h1>OurMenu Page</h1>
+			<Menu/>
 		</div>
 	);
 }
