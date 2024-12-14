558b7f0de306308d1ba09148c6086842b4eb14e1
rday45 <rahularvinddayal@gmail.com>
2024-12-14 22:40:38 +1100

    wrote incomplete test code for cart feature

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,9 +2,49 @@ import { useState, useEffect } from "react";
 import MenuItemCard from "./MenuItemCard";
 import MenuFilter from "./MenuFilter";
 import "../styles/Menu.css";
+// import { jwtDecode } from "jwt-decode";
+
+// Menu cart steps
+// 1. user clicks order
+//     a. add onclick to button
+//     b. console log object information -> console.log quantity and size in the log information
+//         const = {} - create new object
+//     c. create state at top level of menu componet
+//     d. in the button handler setState = setMenuCart([...currentState, newobject])
+// 2. onclick saves quantity = 1 and first size -> opens cart
+// 3. user can select quantity and size (if exists)`
+
+
+
+
 
 export default function Menu() {
   const [menuItems, setMenuItems] = useState([]);
+
+  // This code is breaking the component, figure out why:
+  // const token = localStorage.getItem("jwtToken");
+  // const decodedToken = jwtDecode(token);
+  // console.log(decodedToken);
+  // console.log(token)
+
+  function orderClick(menuItemObj) {
+    console.log(menuItemObj)
+    let defaultSizeAndQuant = {};    
+   
+    if (menuItemObj.multiSizes) {
+      defaultSizeAndQuant = {
+        size:"small",
+        quantity: 1,
+      }
+    } else {
+      defaultSizeAndQuant = {
+        size:"",
+        quantity:1,
+      }
+    }
+    console.log(defaultSizeAndQuant);
+  } 
+
 
   // Each button in the menu filter calls this function to update the menu by category
   async function handleCategoryFilter(category) {
@@ -39,13 +79,11 @@ export default function Menu() {
 
   return (
     <div>
-      <MenuFilter
-        handleCategoryFilter={handleCategoryFilter}
-      ></MenuFilter>
+      <MenuFilter handleCategoryFilter={handleCategoryFilter}></MenuFilter>
       <div className="menu-card-container">
         {menuItems.map((item) => {
           return (
-            <MenuItemCard key={item._id} menuItemObj={item}></MenuItemCard>
+            <MenuItemCard orderClick={orderClick}key={item._id} menuItemObj={item}></MenuItemCard>
           );
         })}
       </div>
