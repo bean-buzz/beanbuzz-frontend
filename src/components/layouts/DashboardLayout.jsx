535a3da25a834a55dbc009e5665f6549fd692113
Chalani Dissanayaka <15415@coderacademy.edu.au>
2024-12-18 22:56:05 +1100

    added removed line

--- src/components/layouts/DashboardLayout.jsx
+++ src/components/layouts/DashboardLayout.jsx
@@ -56,6 +56,7 @@ export default function DashboardLayout() {
         <DashboardNavbar />
         <div className="inner-layout">
           <DashboardSidebar sideitems={sideitems} />
+          {/* Renders children */}
           <Outlet />
         </div>
       </div>
