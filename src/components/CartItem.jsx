3710431de5cc300cd2b8a7a2623597292793fbef
rday45 <rahularvinddayal@gmail.com>
2024-12-18 23:11:25 +1100

    added cart functionality. Increase and decrease quantity and change size.

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,6 +1,13 @@
 import "../styles/CartItem.css";
 
-export default function CartItem({cartItemObj}) {
+export default function CartItem({
+  cartItemObj,
+  removeOrder,
+  increaseQuantity,
+  decreaseQuantity,
+  handleSizeChange,
+  index,
+}) {
   return (
     <div className="cart-item-container">
       <span>
@@ -8,20 +15,43 @@ export default function CartItem({cartItemObj}) {
       </span>
       <span>
         <span className="cart-item-name-container">
-        <h5>{cartItemObj.itemName}</h5>
-        {cartItemObj.size? <h5>({cartItemObj.size})</h5>: null}
+          <h5>{cartItemObj.itemName}</h5>
+          {cartItemObj.size ? <select
+          value={cartItemObj.size}
+          onChange={(event) => handleSizeChange(index, event.target.value)}
+        >
+          <option value="small">Small</option>
+          <option value="medium">Medium</option>
+          <option value="large">Large</option>
+        </select> : null}
         </span>
         <span className="cart-item-quant-price-container">
-        <button className="quantity-button">-</button>
-        <p>{cartItemObj.quantity}</p>
-        <button className="quantity-button">+</button>
-        <p>${cartItemObj.price}</p>
-        <button className="bin-button"><img src="./bin-x.svg" alt="" /></button>
-        
+          <button
+            className="quantity-button"
+            onClick={() => {
+              decreaseQuantity(index);
+            }}
+          >
+            -
+          </button>
+          <p>{cartItemObj.quantity}</p>
+          <button
+            className="quantity-button"
+            onClick={() => increaseQuantity(index)}
+          >
+            +
+          </button>
+          <p>${cartItemObj.price * cartItemObj.quantity}</p>
+          <button
+            className="bin-button"
+            onClick={() => {
+              removeOrder(index);
+            }}
+          >
+            <img src="./bin-x.svg" alt="" />
+          </button>
         </span>
         <p>Instructions: No added sugar please!</p>
-        
-       
       </span>
     </div>
   );
