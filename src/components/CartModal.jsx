9a3f100f9fac23e98701c0f835aa64b85a2c8800
rday45 <rahularvinddayal@gmail.com>
2024-12-22 10:51:35 +1100

    added total cost in cart

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -19,6 +19,11 @@ export default function CartModal({
     }
   };
 
+  let totalCartPrice = cart.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0
+  );
+
   return (
     <div className="modal">
       <div className="overlay" onClick={handleOverlayClick}></div>
@@ -80,6 +85,7 @@ export default function CartModal({
             Checkout
           </button>
         ) : null}
+        <p>Total: ${totalCartPrice}</p>
       </div>
     </div>
   );
